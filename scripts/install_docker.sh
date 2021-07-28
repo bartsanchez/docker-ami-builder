585e98bdec61a8459e0e333ec4aa426be39f8682
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2021-07-28 09:36:03 +0200

    Upgrade to Ubuntu 20.04 LTS in t4g.small

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -3,19 +3,18 @@ set -o errexit
 set -o nounset
 
 sudo apt-get update
-sudo apt-get install -y \
+sudo apt-get install -y\
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
