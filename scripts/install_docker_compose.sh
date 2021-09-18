2637cff647dc4f91096380b2209243bcd60044ba
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2021-09-18 13:59:31 +0200

    Install docker-compose from package

--- scripts/install_docker_compose.sh
+++ scripts/install_docker_compose.sh
@@ -2,10 +2,7 @@
 set -o errexit
 set -o nounset
 
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sudo apt-get update
+sudo apt install -y docker-compose
 
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose"
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose"
+docker-compose --version
