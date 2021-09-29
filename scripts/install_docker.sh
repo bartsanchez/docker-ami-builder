863634e101d0788032e996d9dbfcf371a503e9e5
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2021-09-29 21:51:50 +0200

    Update Docker installation instructions for amd64

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -13,8 +13,8 @@ sudo apt-get install -y\
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
-  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
